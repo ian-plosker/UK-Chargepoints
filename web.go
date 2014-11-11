8870fe6104bf2a4560c293d69c129a987728294b
Ian Plosker <ian.plosker@gmail.com>
2014-11-11 17:21:53 +0000

    Move static content into static dir

--- web.go
+++ web.go
@@ -25,7 +25,7 @@ type Results struct {
 }
 
 func main() {
-	web.Config.StaticDir = "."
+	web.Config.StaticDir = "static"
 	port := os.Getenv("PORT")
 	log.Printf("Listening on port %v ...", port)
 	web.Get("/api/([^/]+/?)", search)
